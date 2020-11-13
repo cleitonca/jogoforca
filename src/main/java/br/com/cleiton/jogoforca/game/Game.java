1cd2a139963e89cb32339a70d19984397d43b286
Cleiton Araújo <cltn.info@gmail.com>
2020-11-12 21:58:57 -0300

    Classe Word

--- src/main/java/br/com/cleiton/jogoforca/game/Game.java
+++ src/main/java/br/com/cleiton/jogoforca/game/Game.java
@@ -1,9 +1,20 @@
 package br.com.cleiton.jogoforca.game;
 
+import br.com.cleiton.jogoforca.core.Word;
+
 public class Game {
 	
 	public void start()
 	{
+		Word word = new Word("casa");
+		
+		word.hasChar('a');
+		word.hasChar('c');
+		word.hasChar('s');
+		
+		System.out.println(word.discovered());
+		
+		System.out.println(word);
 		
 	}
 
